34cc2e0a0bc4eaa4cf6527a7336cb7706bd8a739
vinayakgarole <vinayakgarole10@gamil.com>
2023-04-07 18:15:23 +0530

    Welcome to My Spring Boot Application Project

---
+++ src/main/java/com/bridgelabz/SpringDemo/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.SpringDemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/Vinayak")
+    public String sayHello() {
+        return "Hello Bridgelabz";
+    }
+}
